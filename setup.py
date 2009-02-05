51803585c884ae2d40168de3651ce7b2a6a318a1
Aurynn Shaw <ashaw@commandprompt.com>
2009-02-05 21:49:24 +0000

    Adding new .egg distfiles.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='Simpycity',
-    version='0.2.0a1',
+    version='0.2.0a2',
     description='A simple functionally-oriented PostgreSQL DB access pattern.',
     author='Aurynn Shaw, Commandprompt, Inc.',
     author_email='ashaw@commandprompt.com',
