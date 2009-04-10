64ff4d1428f9ea5ef553bf0c27858915e3f62bde
Lacey Powers <lacey.powers@commandprompt.com>
2009-04-10 23:17:49 +0000

    Updating egg to 0.2.0a10.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='Simpycity',
-    version='0.2.0a9',
+    version='0.2.0a10',
     description='A simple functionally-oriented PostgreSQL DB access pattern.',
     author='Aurynn Shaw, Commandprompt, Inc.',
     author_email='ashaw@commandprompt.com',
