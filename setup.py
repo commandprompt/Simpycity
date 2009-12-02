5b414f864213cd58b952e2ea74befa47e149c84c
Lacey Powers <lacey.powers@commandprompt.com>
2009-12-02 06:19:29 +0000

    Iterating version number to avoid issues in the future.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='Simpycity',
-    version='0.3.0',
+    version='0.3.0a1',
     description='A simple functionally-oriented PostgreSQL DB access pattern.',
     author='Aurynn Shaw, Commandprompt, Inc.',
     author_email='ashaw@commandprompt.com',
