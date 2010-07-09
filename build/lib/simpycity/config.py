750f5469ce13bf6ed440f77ee39e1263b8cd0e4b
Aurynn Shaw <ashaw@commandprompt.com>
2010-07-09 02:27:47 +0000

    Removing build/ directory, which should not have been added to the repo.

--- build/lib/simpycity/config.py
+++
@@ -1,6 +0,0 @@
-port=5432
-host="localhost"
-database=None
-user=None
-password=None
-debug=False
--- build/lib/test/sql/test_unload.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
-DROP TABLE test_table CASCADE;
-
-DROP FUNCTION update_row(int, text);
-
-COMMIT;
