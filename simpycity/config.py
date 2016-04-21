2d04367497504f9e13f206883a1f2645ebde7eaa
Eric Worden <eric@commandprompt.com>
2016-04-21 13:05:04 -0700

    config and dsn() make no default assumptions

--- simpycity/config.py
+++ simpycity/config.py
@@ -1,5 +1,5 @@
-port=5432
-host="localhost"
+port=None
+host=None
 database=None
 user=None
 password=None
@@ -7,8 +7,12 @@ debug=False
 
 
 def dsn():
-    return "host=%s port=%s dbname=%s user=%s password=%s" % \
-           (host, port, database, user, password)
+    configs = {'host': host,
+               'port': port,
+               'dbname': database,
+               'user': user,
+               'password': password}
+    return ' '.join(['{0}={1}'.format(_[0], _[1]) for _ in configs.iteritems() if _[1] is not None])
 
 def handle_factory(*args, **kwargs):
     from handle import Handle
