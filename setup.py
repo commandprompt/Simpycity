9ce039e4bdaf3503ce7e5fb764e48d14cfe35a63
Eric Worden <eric@commandprompt.com>
2016-06-25 17:49:23 -0700

    updated setup.py for version 2.0.0

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ except ImportError:
 
 setup(
     name='Simpycity',
-    version='2.0',
+    version='2.0.0',
     description='A database-respecting object-relational mapper for PostgreSQL.',
-    author='Commandprompt, Inc.',
+    long_description="""Simpycity is an object-relational mapper. It seamlessly maps PostgreSQL query and function result sets to Python classes and class attributes. It allows for the easy and rapid development of query- and stored procedure-based data representations. Simpycity leverages PostgreSQL's powerful composite type system, and the advanced type handling of the psycopg2 database access library.""",
+    author='Command Prompt, Inc.',
     author_email='support@commandprompt.com',
     url='https://github.com/commandprompt/Simpycity',
-    #FIXME
-    download_url='',
+    download_url='https://github.com/commandprompt/Simpycity/releases/tag/2.0.0',
     install_requires=[
         "psycopg2>=2.5"
     ],
@@ -23,12 +23,17 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-        "Operating System :: MacOS X",
-        "Operating System :: Unix",
-        "Topic :: Software Development :: Databases",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Database",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Natural Language :: English'
     ],
+    keywords='orm postgresql',
 )
 
